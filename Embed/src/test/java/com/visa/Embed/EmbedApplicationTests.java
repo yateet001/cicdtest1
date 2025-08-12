ca5f085b25d3f561373b04e625ce396fb5fdce92
manideep <manid@maqsoftware.com>
2025-08-12 12:21:43 +0530

    Initialize Embed application with Maven setup and basic structure

---
+++ Embed/src/test/java/com/visa/Embed/EmbedApplicationTests.java
@@ -0,0 +1,12 @@
+package com.visa.Embed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
